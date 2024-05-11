1befed25e3313c4b8cf4e79c5d3521febf343c07
HTD06 <tiavina.dunn@gmail.com>
2024-05-11 13:20:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,180 +1,65 @@
-var display = document.getElementById("screen");
-var buttons = document.getElementsByClassName("button");
-  
-  Array.prototype.forEach.call(buttons, function(button) {
-  button.addEventListener("click", function() {
-    if (button.textContent != "=" && 
-    button.textContent != "C" && 
-    button.textContent != "x" && 
-    button.textContent != "÷" && 
-    button.textContent != "√" && 
-    button.textContent != "x ²" && 
-    button.textContent != "%" && 
-    button.textContent != "<=" && 
-    button.textContent != "±" && 
-    button.textContent != "sin" && 
-    button.textContent != "cos" && 
-    button.textContent != "tan" && 
-    button.textContent != "log" && 
-    button.textContent != "ln" && 
-    button.textContent != "x^" && 
-    button.textContent != "x !" && 
-    button.textContent != "π" && 
-    button.textContent != "e" && 
-    button.textContent != "rad" 
-    && button.textContent != "∘") {
-      display.value += button.textContent;
-    } else if (button.textContent === "=") {
-      equals();
-    } else if (button.textContent === "C") {
-      clear();
-    } else if (button.textContent === "x") {
-      multiply();
-    } else if (button.textContent === "÷") {
-      divide();
-    } else if (button.textContent === "±") {
-      plusMinus();
-    } else if (button.textContent === "<=") {
-      backspace();
-    } else if (button.textContent === "%") {
-      percent();
-    } else if (button.textContent === "π") {
-      pi();
-    } else if (button.textContent === "x ²") {
-      square();
-    } else if (button.textContent === "√") {
-      squareRoot();
-    } else if (button.textContent === "sin") {
-      sin();
-    } else if (button.textContent === "cos") {
-      cos();
-    } else if (button.textContent === "tan") {
-      tan();
-    } else if (button.textContent === "log") {
-      log();
-    } else if (button.textContent === "ln") {
-      ln();
-    } else if (button.textContent === "x^") {
-      exponent();
-    } else if (button.textContent === "x !") {
-      factorial();
-    } else if (button.textContent === "e") {
-      exp();
-    } else if (button.textContent === "rad") {
-      radians();
-    } else if (button.textContent === "∘") {
-      degrees();
-    }
-  });
-});
-
-
-function syntaxError() {
-  if (eval(display.value) == SyntaxError || eval(display.value) == ReferenceError || eval(display.value) == TypeError) {
-    display.value == "Syntax Error";
-  }
+<script>
+function add_value(input, character) {
+	if(input.value == null || input.value == "0")
+		input.value = character
+	else
+		input.value += character
 }
-
-
-function equals() {
-  if ((display.value).indexOf("^") > -1) {
-    var base = (display.value).slice(0, (display.value).indexOf("^"));
-    var exponent = (display.value).slice((display.value).indexOf("^") + 1);
-    display.value = eval("Math.pow(" + base + "," + exponent + ")");
-  } else {
-    display.value = eval(display.value)
-    checkLength()
-    syntaxError()
-  }
+ 
+function cos(form) {
+	form.display.value = Math.cos(form.display.value);
 }
-
-function clear() {
-  display.value = "";
+ 
+function sin(form) {
+	form.display.value = Math.sin(form.display.value);
 }
-
-function backspace() {
-  display.value = display.value.substring(0, display.value.length - 1);
+ 
+function tan(form) {
+	form.display.value = Math.tan(form.display.value);
 }
-
-function multiply() {
-  display.value += "*";
+ 
+function sqrt(form) {
+	form.display.value = Math.sqrt(form.display.value);
 }
-
-function divide() {
-  display.value +=  "/";
+ 
+function ln(form) {
+	form.display.value = Math.log(form.display.value);
 }
-
-function plusMinus() {
-  if (display.value.charAt(0) === "-") {
-    display.value = display.value.slice(1);
-  } else {
-    display.value = "-" + display.value;
-  }
+ 
+function exp(form) {
+	form.display.value = Math.exp(form.display.value);
 }
-
-function factorial() {
-  var number = 1;
-  if (display.value === 0) {
-    display.value = "1";
-  } else if (display.value < 0) {
-    display.value = "undefined";
-  } else {
-    var number = 1;
-    for (var i = display.value; i > 0; i--) {
-      number *=  i;
-    }
-    display.value = number;
-  }
+ 
+function deleteChar(input) {
+	input.value = input.value.substring(0, input.value.length - 1)
 }
-
-function pi() {
-  display.value = (display.value * Math.PI);
+ 
+function changeSign(input) {
+	if(input.value.substring(0, 1) == "-")
+		input.value = input.value.substring(1, input.value.length)
+	else
+		input.value = "-" + input.value
 }
-
-function square() {
-  display.value = eval(display.value * display.value);
+ 
+function compute(form) {
+	form.display.value = eval(form.display.value)
 }
-
-function squareRoot() {
-  display.value = Math.sqrt(display.value);
+ 
+function square(form) {
+	form.display.value = eval(form.display.value) * eval(form.display.value)
 }
-
-function percent() {
-  display.value = display.value / 100;
+ 
+function checkNum(str) {
+	for (var i = 0; i < str.length; i++) {
+		var ch = str.substring(i, i+1)
+		if (ch < "0" || ch > "9") {
+			if (ch != "/" && ch != "*" && ch != "+" && ch != "-" && ch != "."
+				&& ch != "(" && ch!= ")") {
+				alert("invalid entry!")
+				return false
+				}
+			}
+		}
+		return true
 }
-
-function sin() {
-  display.value = Math.sin(display.value);
-}
-
-function cos() {
-  display.value = Math.cos(display.value);
-}
-
-function tan() {
-  display.value = Math.tan(display.value);
-}
-
-function log() {
-  display.value = Math.log10(display.value);
-}
-
-function ln() {
-  display.value = Math.log(display.value);
-}
-
-function exponent() {
-  display.value += "^";
-}
-
-function exp() {
-  display.value = Math.exp(display.value);
-}
-
-function radians() {
-  display.value = display.value * (Math.PI / 180);
-}
-
-function degrees() {
-  display.value = display.value * (180 / Math.PI);
-}
+</script>
